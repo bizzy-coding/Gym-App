728efd89c530bb857154fee1f4bcdcbde82491de
Bizzy-coding <bizzycoding@gmail.com>
2023-03-25 21:10:14 +0000

    initial upload

---
+++ gym react project/src/App.jsx
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <>
+    <h1>Gym app</h1>
+    <ChooseExercise />
+    </>
+    
+  )
+}
+
+
+const exercises = [
+  { title: 'squat', id: 1 },
+  { title: 'benchpress', id: 2 },
+  { title: 'deadlift', id: 3 },
+];
+
+
+function ChooseExercise() {
+
+  const dropDown = document.createElement('select');
+
+  const listExercises = exercises.map(exercise =>
+    <option key={exercise.id}>
+      {exercise.title}
+    </option>
+    )
+
+    return (
+      <select>
+          {listExercises}
+      </select>
+    )
+
+}
